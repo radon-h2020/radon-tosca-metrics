59b638348b32031c11de45f3140354dce266ee15
Stefano Dalla Palma <stefano.dallapalma0@gmail.com>
2020-12-03 15:27:04 +0100

    [refactoring] NINP->NumParameters

---
+++ toscametrics/metrics/num_parameters.py
@@ -0,0 +1,17 @@
+from toscametrics.blueprint_metric import BlueprintMetric
+from toscametrics.utils import key_value_list
+
+
+class NumParameters(BlueprintMetric):
+    """ This class is responsible counts the blueprint's number of parameters"""
+
+    def count(self):
+
+        params = 0
+        for key, value in key_value_list(self.blueprint):
+
+            if key == 'interfaces':
+                for interface, items in value.items():
+                    params += len(items.get('inputs', dict()))
+
+        return params
